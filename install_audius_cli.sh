25fc0f5142f3eb5dce0fedc3915d143ef43869a7
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-03-03 10:47:02 -0500

    Add script to install audius-cli

---
+++ install_audius_cli.sh
@@ -0,0 +1,13 @@
+# install basic deps
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl jq python3.8 python3.8-distutils
+
+# audius-cli init
+sudo ln -sf $(dirname $(readlink -f "$0"))/audius-cli /usr/local/bin/audius-cli
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.8 get-pip.py --force-reinstall
+python3.8 -m pip install --user python-crontab pyyaml
+rm get-pip.py
+
+# backup existing configs
+python3.8 backup.py
