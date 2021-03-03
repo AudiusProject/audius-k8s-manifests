7120c847160ee38da1a4ec8fbe72df5315c55335
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-03-03 11:07:33 -0500

    Add relative path and exec permissions

--- install_audius_cli.sh
+++ install_audius_cli.sh
@@ -3,6 +3,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl jq python3.8 python3.8-distutils
 
 # audius-cli init
+chmod +x $(dirname $(readlink -f "$0"))/audius-cli
 sudo ln -sf $(dirname $(readlink -f "$0"))/audius-cli /usr/local/bin/audius-cli
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.8 get-pip.py --force-reinstall
@@ -10,4 +11,4 @@ python3.8 -m pip install --user python-crontab pyyaml
 rm get-pip.py
 
 # backup existing configs
-python3.8 backup.py
+python3.8 $(dirname $(readlink -f "$0"))/backup.py
