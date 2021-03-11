bd47e66421adf0ce0f20c015186b8d7cc787c4b8
Cheran <cheran.v.senthil@gmail.com>
2021-03-11 09:10:33 -0500

    Add audius-cli to manage configs (#53)
    * Add audius cli
    
    * Update audius-cli
    
    * Update audius-cli
    
    * Update audius-cli
    
    * Add conflicts for fun
    
    * Use kubectl instead of k
    
    * Fix jq issues
    
    * Revert some changes
    
    * Minor fixes for testing
    
    * Some fixes
    
    * Add actual health check
    
    * Change launch flag name
    
    * Fix bug
    
    * Add auto upgrade
    
    * More fixes
    
    * Fix pipe issue
    
    * Update audius-cli
    
    * Fix bug
    
    * Update commit hash
    
    * Fix path issue
    
    * Fix README.md
    
    * Change to no launch
    
    * Add no launch
    
    * Change to not
    
    * update shorthand
    
    * Make debugging regex easier
    
    * Improve backward compatibility
    
    * Add pyyaml to imports
    
    * Remove usage of capture_output
    
    * Force python 3.8
    
    * Fix pip issues
    
    * Add python repos
    
    * Add pull command and comments
    
    * Prompt for user input
    
    * Fix failing health check
    
    * Fix var name
    
    * Add script to install audius-cli
    
    * Update README instructions for audius-cli
    
    * Add backup file for configs for first install
    
    * Add relative path and exec permissions
    
    * Update commit hash
    
    * Bump changes from audius-k8s

---
+++ backup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.8
+
+import os
+import subprocess
+
+import yaml
+
+
+def main():
+    output = {}
+
+    manifests_path = os.getenv("MANIFESTS_PATH") or os.path.expanduser(
+        "~/audius-k8s-manifests"
+    )
+
+    for service in ["creator-node", "discovery-provider"]:
+        config_map = os.path.join(manifests_path, f"audius/{service}/{service}-cm.yaml")
+
+        proc = subprocess.run(
+            f"git show HEAD:audius/{service}/{service}-cm.yaml",
+            capture_output=True,
+            shell=True,
+            check=True,
+            cwd=manifests_path,
+        )
+
+        org_config = {
+            conf["metadata"]["name"]: conf["data"]
+            for conf in yaml.safe_load_all(proc.stdout.decode("utf-8"))
+        }
+        usr_config = {
+            conf["metadata"]["name"]: conf["data"]
+            for conf in yaml.safe_load_all(open(config_map, "r"))
+        }
+
+        output[service] = {}
+        for cm in usr_config:
+            out_cm = cm[len(service) + 1 : -3]
+
+            output[service][out_cm] = {}
+            if cm not in org_config:
+                output[service][out_cm] = usr_config[cm]
+                continue
+
+            for key in {*usr_config[cm].keys(), *org_config[cm].keys()}:
+                if key in usr_config[cm] and key not in org_config[cm]:
+                    output[service][out_cm][key] = usr_config[cm][key]
+                elif key not in usr_config[cm] and key in org_config[cm]:
+                    output[service][out_cm][key] = ""
+                elif usr_config[cm][key] != org_config[cm][key]:
+                    output[service][out_cm][key] = usr_config[cm][key]
+
+    with open(os.path.join(manifests_path, "config.yaml"), "w") as stream:
+        yaml.safe_dump(output, stream)
+
+
+if __name__ == "__main__":
+    main()
