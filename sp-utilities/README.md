fa981350accf0961a5c45dcd5c3dae0108cbc720
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-04-20 17:59:31 -0400

    Bump to version 0.3.34

--- sp-utilities/README.md
+++ sp-utilities/README.md
@@ -112,19 +112,48 @@ cd sp-utilities/claim
 
 Then you can run the following command to setup a cron job. (Be sure to replace the full path to `claim.js`)
 ```
-(crontab -l 2>/dev/null; echo "0 */6 * * * node full/path/to/claim.js <spOwnerWallet> <privateKey>") | crontab -
+(crontab -l 2>/dev/null; echo "0 */6 * * * node full/path/to/claim.js claim-rewards <spOwnerWallet> <privateKey>") | crontab -
 ```
 
 Be sure to replace `full/path/to/claim.js` with the actual path on your local machine.
 
 
 ```
-Usage: claim [options] <spOwnerWallet> <privateKey>
+Usage: claim [options] [command]
+
+Options:
+  -h, --help                                            display help for command
+
+Commands:
+  initiate-round [options] <privateKey>                 Initiates new round for claiming rewards
+  claim-rewards [options] <spOwnerWallet> <privateKey>  Claim rewards for given spOwnerWallet
+  help [command]                                        display help for command
+```
+
+```
+Usage: claim initiate-round [options] <privateKey>
+
+Initiates new round for claiming rewards
 
 Options:
   --eth-registry-address <ethRegistryAddress>  Registry contract address (default: "0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C")
   --eth-token-address <ethTokenAddress>        Token contract address (default: "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998")
   --web3-provider <web3Provider>               Web3 provider to use (default: "https://mainnet.infura.io/v3/a3ed533ddfca4c76ab4df7556e2745e1")
-  --init-round                             initiate new rounds
+  --gas <gas>                                  ammount of gas to use (default: 100000)
+  --gas-price <gasPrice>                       gas price in gwei
   -h, --help                                   display help for command
-```
+  ```
+
+  ```
+  Usage: claim claim-rewards [options] <spOwnerWallet> <privateKey>
+
+Claim rewards for given spOwnerWallet
+
+Options:
+  --eth-registry-address <ethRegistryAddress>  Registry contract address (default: "0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C")
+  --eth-token-address <ethTokenAddress>        Token contract address (default: "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998")
+  --web3-provider <web3Provider>               Web3 provider to use (default: "https://mainnet.infura.io/v3/a3ed533ddfca4c76ab4df7556e2745e1")
+  --gas <gas>                                  ammount of gas to use (default: 1000000)
+  --gas-price <gasPrice>                       gas price in gwei
+  -h, --help                                   display help for command
+  ```
--- sp-utilities/claim/claim.js
+++ sp-utilities/claim/claim.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 const { program } = require('commander')
 
 const audius = require('@audius/libs')
@@ -8,84 +8,130 @@ const defaultRegistryAddress = '0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C'
 const defaultTokenAddress = '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998'
 const defaultWeb3Provider = 'https://mainnet.infura.io/v3/a3ed533ddfca4c76ab4df7556e2745e1'
 
+async function configureLibs(ethRegistryAddress, ethTokenAddress, web3Provider) {
+  const configuredWeb3 = await audius.Utils.configureWeb3(web3Provider, null, false)
 
-const configureLibs = async (account, ethRegistryAddress, ethTokenAddress, web3Provider) => {
-    const configuredWeb3 = await audius.Utils.configureWeb3(
-        web3Provider,
-        null,
-        false,
-    )
+  const audiusLibsConfig = {
+    ethWeb3Config: audius.configEthWeb3(ethTokenAddress, ethRegistryAddress, configuredWeb3, '0x0'),
+    isServer: true,
+  }
 
-    const audiusLibsConfig = {
-        ethWeb3Config: audius.configEthWeb3(
-            ethTokenAddress,
-            ethRegistryAddress,
-            configuredWeb3,
-            account
-        ),
-        isServer: true
-    }
+  const libs = new audius(audiusLibsConfig)
+  await libs.init()
 
-    const libs = new audius(audiusLibsConfig)
-    await libs.init()
-
-    return libs
+  return libs
 }
 
+async function getClaimsManagerContract(ethRegistryAddress, ethTokenAddress, web3) {
+  const audiusLibs = await configureLibs(ethRegistryAddress, ethTokenAddress, web3.eth.currentProvider)
+  await audiusLibs.ethContracts.ClaimsManagerClient.init()
+  return new web3.eth.Contract(
+    audiusLibs.ethContracts.ClaimsManagerClient._contract.options.jsonInterface,
+    audiusLibs.ethContracts.ClaimsManagerClient._contract.options.address
+  )
+}
 
-async function run(spOwnerWallet, privateKey, { ethRegistryAddress, ethTokenAddress, web3Provider, initRound, initiateGas, claimGas }) {
-    const provider = new HDWalletProvider({ privateKeys: [privateKey], providerOrUrl: web3Provider })
-    const web3 = new Web3(provider)
+async function getDelegateManagerContract(ethRegistryAddress, ethTokenAddress, web3) {
+  const audiusLibs = await configureLibs(ethRegistryAddress, ethTokenAddress, web3.eth.currentProvider)
+  await audiusLibs.ethContracts.DelegateManagerClient.init()
+  return new web3.eth.Contract(
+    audiusLibs.ethContracts.DelegateManagerClient._contract.options.jsonInterface,
+    audiusLibs.ethContracts.DelegateManagerClient._contract.options.address
+  )
+}
 
-    const audiusLibs = await configureLibs(
-        spOwnerWallet,
-        ethRegistryAddress,
-        ethTokenAddress,
-        provider,
-    )
+async function initiateRound(privateKey, { ethRegistryAddress, ethTokenAddress, web3Provider, gas, gasPrice }) {
+  const web3 = new Web3(
+    new HDWalletProvider({
+      privateKeys: [privateKey],
+      providerOrUrl: web3Provider,
+    })
+  )
 
-    const claimsManagerContract = audiusLibs.ethContracts.ClaimsManagerClient
-    const delegateContract = audiusLibs.ethContracts.DelegateManagerClient
+  const accountAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address
 
-    const claimPending = await claimsManagerContract.claimPending(spOwnerWallet)
+  const claimsManagerContract = await getClaimsManagerContract(ethRegistryAddress, ethTokenAddress, web3)
 
-    if ((!claimPending) && initRound) {
-        const currentBlock = await web3.eth.getBlockNumber();
-        const lastFundedBlock = await claimsManagerContract.getLastFundedBlock()
-        const blockDiff = currentBlock - lastFundedBlock
-        const requiredBlockDiff = await claimsManagerContract.getFundingRoundBlockDiff()
+  const lastFundedBlock = await claimsManagerContract.methods.getLastFundedBlock().call()
+  const requiredBlockDiff = await claimsManagerContract.methods.getFundingRoundBlockDiff().call()
 
-        if (blockDiff <= requiredBlockDiff) {
-            console.log(`Block difference (${requiredBlockDiff}) not met`)
-            process.exit(1)
-        }
+  const currentBlock = await web3.eth.getBlockNumber()
+  const blockDiff = currentBlock - lastFundedBlock
 
-        console.log('Initializing Round')
-        await claimsManagerContract.initiateRound(0, initiateGas)
-        console.log('Initiated Round')
-    }
+  if (blockDiff <= requiredBlockDiff) {
+    console.log(`Block difference of ${requiredBlockDiff} not met, ${requiredBlockDiff - blockDiff} blocks remaining`)
+    process.exit(1)
+  }
 
-    if (claimPending) {
-        console.log('Claiming Rewards')
-        await delegateContract.claimRewards(spOwnerWallet, 0, claimGas)
-        console.log('Claimed Rewards successfully')
-    }
+  console.log('Initializing Round')
+  await claimsManagerContract.methods.initiateRound().send({
+    from: accountAddress,
+    gas,
+    gasPrice: gasPrice ? web3.utils.toWei(gasPrice, 'gwei') : (await web3.eth.getGasPrice()).toString(),
+  })
+  console.log('Successfully initiated Round')
+}
 
-    process.exit(0)
+async function claimRewards(
+  spOwnerWallet,
+  privateKey,
+  { ethRegistryAddress, ethTokenAddress, web3Provider, gas, gasPrice }
+) {
+  const web3 = new Web3(
+    new HDWalletProvider({
+      privateKeys: [privateKey],
+      providerOrUrl: web3Provider,
+    })
+  )
+
+  const accountAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address
+
+  const claimsManagerContract = await getClaimsManagerContract(ethRegistryAddress, ethTokenAddress, web3)
+  const delegateManagerContract = await getDelegateManagerContract(ethRegistryAddress, ethTokenAddress, web3)
+
+  const claimPending = await claimsManagerContract.methods.claimPending(spOwnerWallet).call()
+
+  if (claimPending) {
+    console.log('Claiming Rewards')
+    await delegateManagerContract.methods.claimRewards(spOwnerWallet).send({
+      from: accountAddress,
+      gas,
+      gasPrice: gasPrice ? web3.utils.toWei(gasPrice, 'gwei') : (await web3.eth.getGasPrice()).toString(),
+    })
+    console.log('Claimed Rewards successfully')
+  } else {
+    console.log('No claims pending')
+  }
 }
 
 async function main() {
-    program
-        .arguments('<spOwnerWallet> <privateKey>')
-        .option('--eth-registry-address <ethRegistryAddress>', 'Registry contract address', defaultRegistryAddress)
-        .option('--eth-token-address <ethTokenAddress>', 'Token contract address', defaultTokenAddress)
-        .option('--web3-provider <web3Provider>', 'Web3 provider to use', defaultWeb3Provider)
-        .option('--init-round', 'Initiate new rounds')
-        .option('--claim-gas', 'Gas for claiming', 1500000)
-        .option('--initiate-gas', 'Gas for Initiating new rounds', 100000)
-        .action(run)
+  program
+    .command('initiate-round <privateKey>')
+    .description('Initiates new round for claiming rewards')
+    .option('--eth-registry-address <ethRegistryAddress>', 'Registry contract address', defaultRegistryAddress)
+    .option('--eth-token-address <ethTokenAddress>', 'Token contract address', defaultTokenAddress)
+    .option('--web3-provider <web3Provider>', 'Web3 provider to use', defaultWeb3Provider)
+    .option('--gas <gas>', 'ammount of gas to use', 100000)
+    .option('--gas-price <gasPrice>', 'gas price in gwei')
+    .action(initiateRound)
 
+  program
+    .command('claim-rewards <spOwnerWallet> <privateKey>')
+    .description('Claim rewards for given spOwnerWallet')
+    .option('--eth-registry-address <ethRegistryAddress>', 'Registry contract address', defaultRegistryAddress)
+    .option('--eth-token-address <ethTokenAddress>', 'Token contract address', defaultTokenAddress)
+    .option('--web3-provider <web3Provider>', 'Web3 provider to use', defaultWeb3Provider)
+    .option('--gas <gas>', 'ammount of gas to use', 1000000)
+    .option('--gas-price <gasPrice>', 'gas price in gwei')
+    .action(claimRewards)
+
+  try {
     await program.parseAsync(process.argv)
+    process.exit(0)
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
 }
 
 main()
