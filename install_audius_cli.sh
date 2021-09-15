d1f32ec209fb4850c17b04db327a1efe0347ba2b
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-09-15 16:42:53 -0400

    Bump to release v0.3.44

--- install_audius_cli.sh
+++ install_audius_cli.sh
@@ -7,7 +7,7 @@ chmod +x $(dirname $(readlink -f "$0"))/audius-cli
 sudo ln -sf $(dirname $(readlink -f "$0"))/audius-cli /usr/local/bin/audius-cli
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.8 get-pip.py --force-reinstall
-python3.8 -m pip install --user python-crontab pyyaml
+python3.8 -m pip install --user python-crontab pyyaml psutil
 rm get-pip.py
 
 # backup existing configs
