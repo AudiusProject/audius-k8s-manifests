ec286a553fc9234cedb9131b382c265cc5442237
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-02-20 18:57:26 -0500

    add post-pull.sh comment

--- post-pull.sh
+++ post-pull.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo from post-pull.sh
+# removed in release-v0.3.52 when we removed the IPFS dependency
+# https://github.com/AudiusProject/audius-protocol/commit/030b1d0efba337b10c5095b90f369712d2604257
 kubectl delete deployment discovery-provider-ipfs
