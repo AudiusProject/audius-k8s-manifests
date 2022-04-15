d45dad90a7d461ac6f28f6912fd7ecb0da994309
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-04-15 16:28:19 -0500

    Bump to v0.3.56

--- post-pull.sh
+++ post-pull.sh
@@ -3,4 +3,6 @@
 echo from post-pull.sh
 # removed in release-v0.3.52 when we removed the IPFS dependency
 # https://github.com/AudiusProject/audius-protocol/commit/030b1d0efba337b10c5095b90f369712d2604257
-kubectl delete deployment discovery-provider-ipfs
+if kubectl get deployments | grep discovery-provider-ipfs > /dev/null; then
+  kubectl delete deployment discovery-provider-ipfs
+fi
